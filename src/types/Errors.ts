08a4d46406d77c0a95c0eadb868870736c422e1f
SeanningTatum <seantheurgel@gmail.com>
2021-04-10 08:41:01 +0800

    remove code

--- src/types/Errors.ts
+++ src/types/Errors.ts
@@ -1,60 +1,9 @@
 /* eslint-disable class-methods-use-this */
-export class ServerError extends Error {
-  constructor() {
-    super('A server error has occurred');
-    this.name = 'ServerError';
-  }
-}
+// export class ServerError extends Error {
+//   constructor() {
+//     super('A server error has occurred');
+//     this.name = 'ServerError';
+//   }
+// }
 
-export class BadRequestError extends Error {
-  constructor() {
-    super(`Something was wrong with your request body`);
-    this.name = 'BadRequestError';
-  }
-}
-
-export class NotAuthorizedError extends Error {
-  constructor() {
-    super('User is not authorized to view this file!');
-    this.name = 'NotAuthorizedError';
-  }
-}
-export class NotFoundError extends Error {
-  constructor() {
-    super(`Entity is not found!`);
-    this.name = 'NotFoundError';
-  }
-}
-
-export class InsufficientFundsError extends Error {
-  constructor() {
-    super(`You don't have enough money brah!`);
-    this.name = 'PayPay InsufficientFunds';
-  }
-}
-
-export class InvalidCardError extends Error {
-  constructor() {
-    super(`You don't have enough money brah!`);
-    this.name = 'PayPay InsufficientFunds';
-  }
-}
-
-export class RestrictedRegionError extends Error {
-  constructor() {
-    super(`You don't have enough money brah!`);
-    this.name = 'PayPay InsufficientFunds';
-  }
-
-  listValidCountries(): string {
-    return 'hi';
-  }
-}
-
-export type PayPalErrors =
-  | InsufficientFundsError
-  | InvalidCardError
-  | RestrictedRegionError
-  | any;
-
-export type AllErrors = ServerError | NotAuthorizedError | NotFoundError;
+// export type AllErrors = ServerError;
--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,68 +1,28 @@
 import axios from 'axios';
 
-import {
-  BadRequestError,
-  NotAuthorizedError,
-  NotFoundError,
-} from '~/types/Errors';
-
-const DEFAULT_HEADERS: Record<string, string> = {
-  Accept: 'application/json',
-  'Content-Type': 'application/json',
-};
+// const DEFAULT_HEADERS: Record<string, string> = {
+//   Accept: 'application/json',
+//   'Content-Type': 'application/json',
+// };
 
 const instance = axios.create({
   // baseURL: process.env.REACT_APP_API_URL,
   // headers: DEFAULT_HEADERS,
 });
 
-instance.interceptors.request.use((request) => request);
-
-instance.interceptors.response.use(
+axios.interceptors.response.use(
   (response) =>
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     response,
   (error) => {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      // console.log('Server Error', error.response);
-
-      if (error.response.data.code === 400) {
-        throw new BadRequestError();
-      }
-
-      if (error.response.data.code === 401) {
-        throw new NotAuthorizedError();
-      }
-
-      if (error.response.data.code === 404) {
-        throw new NotFoundError();
-      }
-
-      return Promise.reject({
-        responseType: 'server',
-        ...error.response.data,
-      });
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    if (error.response.data) {
+      const { code } = error.response.data;
     }
-    if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-      // http.ClientRequest in node.js
-      console.log('Request Error', error.response);
-      return Promise.reject({
-        responseType: 'request',
-        message: error.message,
-      });
-    }
-    // Something happened in setting up the request that triggered an Error
-    console.log('Generic Error', error.message);
 
-    return Promise.reject({
-      responseType: 'generic',
-      message: error.message,
-    });
+    return Promise.reject(error);
   },
 );
 
